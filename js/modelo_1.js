dd4fd8dbf5328602cf3f426c1b7b540bcb335120
JOAO <cg3015297@LAB204M11.IFSP>
2023-06-20 08:42:54 -0300

    first commit

---
+++ js/modelo_1.js
@@ -0,0 +1,44 @@
+let modeloJson = [
+    {
+      id: 1,
+      name: 'A Freira',
+      img: 'img/a freira.jpg',
+      price: 2018,
+      description: 'Presa em um convento na Romênia, uma freira comete suicídio. Para investigar o caso, o Vaticano envia um padre assombrado e uma noviça prestes a se tornar freira. Arriscando suas vidas, a fé e até suas almas, os dois descobrem um segredo profano e se confrontam com uma força do mal que toma a forma de uma freira demoníaca e transforma o convento em um campo de batalha.',
+    },
+    {
+      id: 2,
+      name: 'Invocação do Mal',
+      img: 'img/idm.jpg',
+      price: 2013,
+      description: 'Os investigadores paranormais Ed e Lorraine Warren trabalham para ajudar a família aterrorizada por uma entidade demoníaca em sua fazenda.',
+    },
+    {
+      id: 3,
+      name: 'Invocação do Mal 2',
+      img: 'img/idm2.jpg',
+      price: 2016,
+      description: 'Os famosos demonologistas Lorraine e Ed Warren viajam até o norte de Londres. Lá, eles ajudam uma mãe solteira que cria quatro filhos sozinha em uma casa atormentada por espíritos malignos.',
+    },
+    {
+      id: 4,
+      name: 'Invocação do Mal 3',
+      img: 'img/idm3.jpg',
+      price: 2021,
+      description: 'Os investigadores paranormais Ed e Lorraine Warren assumem um dos casos mais sensacionais de suas carreiras quando um jovem suspeito de assassinato alega posse demoníaca como defesa.',
+    },
+    {
+      id: 5,
+      name: 'A Maldição da Chorona',
+      img: 'img/chorona.jpg',
+      price: 2019,
+      description: 'Em Los Angeles, na década de 1970, uma assistente social cria seus dois filhos sozinha depois de ser deixada viúva. Ela começa a ver semelhanças entre um caso que está investigando e a entidade sobrenatural chamada de Chorona. A lenda conta que, em vida, Chorona afogou seus filhos após um ataque de ciúmes e depois se jogou no rio, se debulhando em lágrimas. Agora ela chora eternamente, indo atrás e capturando outras crianças para substituir seus filhos.',
+    },
+    {
+      id: 6,
+      name: 'Annabelle',
+      img: 'img/an.jpg',
+      price: 2014,
+      description: 'John Form acha que encontrou o presente ideal para sua esposa grávida, uma boneca vintage. No entanto, a alegria do casal não dura muito. Uma noite terrível, membros de uma seita satânica invadem a casa do casal em um ataque violento. Ao tentarem invocar um demônio, eles mancham a boneca de sangue, tornando-a receptora de uma entidade do mal.',
+    },
+  ];
